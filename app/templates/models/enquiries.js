1713551f78ec7ba0c5cc512976596316595d237d
Jed Watson <jed.watson@me.com>
2014-02-25 16:42:11 +1100

    Adding send-notification functionality to enquiries

--- app/templates/models/enquiries.js
+++ app/templates/models/enquiries.js
@@ -18,7 +18,40 @@ Enquiry.add({
 	message: { type: Types.Markdown, required: true },
 	createdAt: { type: Date, default: Date.now }
 });
+<% if (includeEmail) { %>
+Enquiry.schema.pre('save', function(next) {
+	this.wasNew = this.isNew;
+	next();
+})
 
+Enquiry.schema.post('save', function() {
+	if (this.wasNew) {
+		this.sendNotificationEmail();
+	}
+});
+
+Enquiry.schema.methods.sendNotificationEmail = function(callback) {
+	
+	var enqiury = this;
+	
+	keystone.list('User').model.find().where('isAdmin', true).exec(function(err, admins) {
+		
+		if (err) return callback(err);
+		
+		new keystone.Email('enquiry-notification').send({
+			to: admins,
+			from: {
+				name: '<%= projectName %>',
+				email: 'contact@<%= _.slugify(projectName) %>.com'
+			},
+			subject: 'New Enquiry for <%= projectName %>',
+			enquiry: enqiury
+		}, callback);
+		
+	});
+	
+}
+<% } %>
 Enquiry.defaultSort = '-createdAt';
 Enquiry.defaultColumns = 'name, email, enquiryType, createdAt';
 Enquiry.register();
