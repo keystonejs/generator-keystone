7200a7fe1b54974fd7d501438daa5998119fbfe0
Jed Watson <jed.watson@me.com>
2014-04-14 01:03:43 +1000

    Improving the Post model

--- app/templates/models/posts.js
+++ app/templates/models/posts.js
@@ -8,10 +8,9 @@ var Post = new keystone.List('Post', {
 
 Post.add({
 	title: { type: String, required: true },
-	slug: { type: String, index: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
-	publishedDate: { type: Types.Date, index: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage },
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
