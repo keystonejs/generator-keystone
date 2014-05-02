7e4854f66e23ab19090803f4abb81a69471484e8
Jed Watson <jed.watson@me.com>
2014-05-02 18:20:28 +1000

    Adding unique keys for Post Categories

--- app/templates/models/postCategories.js
+++ app/templates/models/postCategories.js
@@ -2,7 +2,7 @@ var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
 var PostCategory = new keystone.List('PostCategory', {
-	autokey: { from: 'name', path: 'key' }
+	autokey: { from: 'name', path: 'key', unique: true }
 });
 
 PostCategory.add({
