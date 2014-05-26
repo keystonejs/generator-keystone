6740814f9b1212a1947de6f76bda8ea179d3f823
Jed Watson <jed.watson@me.com>
2014-05-26 18:20:11 +1000

    Using new (simple!) technique in the update script template

--- app/templates/updates/0.0.1-admins.js
+++ app/templates/updates/0.0.1-admins.js
@@ -1,3 +1,21 @@
+/**
+ * This script automatically creates a default Admin user when an
+ * empty database is used for the first time. You can use this
+ * technique to insert data into any List you have defined.
+ */
+
+exports.create = {
+	User: [
+		{ email: 'user@keystonejs.com', password: 'admin', name: { first: 'Admin', last: 'User' }, isAdmin: true }
+	]
+};
+
+/**
+ * The following is the older version of this update script, it is
+ * left here for reference as an example of how more complex updates
+ * can be structured.
+ */
+/*
 var keystone = require('keystone'),
 	async = require('async'),
 	User = keystone.list('User');
@@ -26,3 +44,4 @@ function createAdmin(admin, done) {
 exports = module.exports = function(done) {
 	async.forEach(admins, createAdmin, done);
 };
+*/
