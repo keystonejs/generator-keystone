098b605d8b31311c7164c0c47bef076c0290d356
Jed Watson <jed.watson@me.com>
2014-05-26 17:51:35 +1000

    Added hint about locals.section use

--- app/templates/routes/views/index.js
+++ app/templates/routes/views/index.js
@@ -5,7 +5,8 @@ exports = module.exports = function(req, res) {
 	var locals = res.locals,
 		view = new keystone.View(req, res);
 	
-	// Set locals
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
 	locals.section = 'home';
 	
 	// Render the view
