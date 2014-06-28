14b2c2026f09c5399f71dabac7493dc9737d26b4
Jed Watson <jed.watson@me.com>
2014-06-29 00:29:19 +1000

    Cleaning up formatting

--- app/index.js
+++ app/index.js
@@ -7,19 +7,19 @@ var util = require('util'),
 
 
 var KeystoneGenerator = module.exports = function KeystoneGenerator(args, options, config) {
-
+	
 	// Initialise default values
 	this.cloudinaryURL = false;
 	this.mandrillAPI = false;
-
-	this.messages = [];
-
+	
 	// Apply the Base Generator
 	yeoman.generators.Base.apply(this, arguments);
-
-	// Init Messages
+	
+	// Welcome
 	console.log('\nWelcome to KeystoneJS.\n');
-
+	
+	// This callback is fired when the generator has completed,
+	// and includes instructions on what to do next.
 	var done = _.bind(function done() {
 		console.log(
 			'\n------------------------------------------------' +
@@ -44,35 +44,35 @@ var KeystoneGenerator = module.exports = function KeystoneGenerator(args, option
 
 			'\n\nTo start your new website, run "node keystone".' +
 			'\n');
-
+		
 	}, this);
-
+	
 	// Install Dependencies when done
 	this.on('end', function () {
-
+		
 		this.installDependencies({
 			bower: false,
 			skipMessage: true,
 			skipInstall: options['skip-install'],
 			callback: done
 		});
-
+		
 	});
-
+	
 	// Import Package.json
 	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-
+	
 };
 
 // Extends the Base Generator
 util.inherits(KeystoneGenerator, yeoman.generators.Base);
 
 KeystoneGenerator.prototype.prompts = function prompts() {
-
+	
 	var cb = this.async();
-
+	
 	var prompts = {
-
+		
 		project: [
 			{
 				name: 'projectName',
@@ -122,17 +122,17 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 				default: true
 			}
 		],
-
+		
 		config: []
-
+		
 	};
-
+	
 	this.prompt(prompts.project, function(props) {
-
+		
 		_.each(props, function(val, key) {
 			this[key] = val;
 		}, this);
-
+		
 		// Keep an unescaped version of the project name
 		this._projectName = this.projectName;
 		// ... then escape it for use in strings (most cases)
@@ -148,9 +148,9 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 		
 		this.userModelPath = utils.keyToPath(this.userModel, true);
 		this.taskRunner = (this.taskRunner || '').toLowerCase().trim();
-
+		
 		if (this.includeBlog || this.includeGallery || this.includeEmail) {
-
+			
 			if (this.includeEmail) {
 				prompts.config.push({
 					name: 'mandrillAPI',
@@ -163,17 +163,17 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 						'\n    Your Mandrill API Key:'
 				});
 			}
-
+			
 			if (this.includeBlog || this.includeGallery) {
-
+				
 				var blog_gallery = 'blog and gallery templates';
-
+				
 				if (!this.includeBlog) {
 					blog_gallery = 'gallery template';
 				} else if (!this.includeGallery) {
 					blog_gallery = 'blog template';
 				}
-
+				
 				prompts.config.push({
 					name: 'cloudinaryURL',
 					message: '------------------------------------------------' +
@@ -186,43 +186,43 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 						'\n    ' +
 						'\n    Please enter your Cloudinary URL:'
 				});
-
+				
 			}
-
+			
 		}
-
+		
 		if (!prompts.config.length) {
 			return cb();
 		}
-
+		
 		this.prompt(prompts.config, function(props) {
-
+			
 			_.each(props, function(val, key) {
 				this[key] = val;
 			}, this);
-
+			
 			if (this.includeEmail && !this.mandrillAPI) {
 				this.usingTestMandrillAPI = true;
 				this.mandrillAPI = 'NY8RRKyv1Bure9bdP8-TOQ';
 			}
-
+			
 			if (!this.cloudinaryURL && (this.includeBlog || this.includeGallery)) {
 				this.usingDemoCloudinaryAccount = true;
 				this.cloudinaryURL = 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo';
 			}
-
+			
 			cb();
-
+			
 		}.bind(this));
-
+		
 	}.bind(this));
-
+	
 };
 
 KeystoneGenerator.prototype.guideComments = function() {
-
+	
 	var cb = this.async();
-
+	
 	this.prompt([
 		{
 			type: 'confirm',
@@ -233,133 +233,132 @@ KeystoneGenerator.prototype.guideComments = function() {
 			default: true
 		}
 	], function(props) {
-
+		
 		this.includeGuideComments = props.includeGuideComments;
 		cb();
-
+		
 	}.bind(this));
-
+	
 };
 
 KeystoneGenerator.prototype.keys = function keys() {
-
+	
 	var cookieSecretChars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz!@#$%^&*()-=_+[]{}|;:",./<>?`~';
-
+	
 	this.cookieSecret = utils.randomString(64, cookieSecretChars);
-
+	
 };
 
 KeystoneGenerator.prototype.project = function project() {
-
+	
 	this.template('_package.json', 'package.json');
 	this.template('_env', '.env');
 	this.template('_jshintrc', '.jshintrc');
-
+	
 	this.template('_keystone.js', 'keystone.js');
-
+	
 	this.copy('editorconfig', '.editorconfig');
 	this.copy('gitignore', '.gitignore');
 	this.copy('Procfile');
-
+	
 	if(this.taskRunner === 'grunt') {
 		this.copy('Gruntfile.js');
 	}
-
+	
 	if(this.taskRunner === 'gulp'){
 		this.copy('gulpfile.js');
 	}
-
-
+	
 };
 
 KeystoneGenerator.prototype.models = function models() {
-
+	
 	var modelFiles = [],
 		modelIndex = '';
-
+	
 	if (this.includeBlog) {
 		modelFiles.push('Post');
 		modelFiles.push('PostCategory');
 	}
-
+	
 	if (this.includeGallery) {
 		modelFiles.push('Gallery');
 	}
-
+	
 	if (this.includeEnquiries) {
 		modelFiles.push('Enquiry');
 	}
-
+	
 	this.mkdir('models');
-
+	
 	this.template('models/_user.js', 'models/'+this.userModel+'.js');
-
+	
 	modelFiles.forEach(function(i) {
 		this.template('models/' + i + '.js');
 		modelIndex += 'require(\'./' + i + '\');\n';
 	}, this);
-
+	
 	// we're now using keystone.import() for loading models, so an index.js
 	// file is no longer required. leaving for reference.
-
+	
 	// this.write('models/index.js', modelIndex);
-
+	
 };
 
 KeystoneGenerator.prototype.routes = function routes() {
-
+	
 	this.mkdir('routes');
 	this.mkdir('routes/views');
-
+	
 	this.template('routes/_index.js', 'routes/index.js');
 	this.template('routes/_middleware.js', 'routes/middleware.js');
-
+	
 	if (this.includeEmail) {
 		this.template('routes/_emails.js', 'routes/emails.js');
 	}
-
+	
 	this.copy('routes/views/index.js');
-
+	
 	if (this.includeBlog) {
 		this.copy('routes/views/blog.js');
 		this.copy('routes/views/post.js');
 	}
-
+	
 	if (this.includeGallery) {
 		this.copy('routes/views/gallery.js');
 	}
-
+	
 	if (this.includeEnquiries) {
 		this.copy('routes/views/contact.js');
 	}
-
+	
 };
 
 KeystoneGenerator.prototype.templates = function templates() {
-
+	
 	if (this.viewEngine === 'hbs') {
 		
 		// Copy Handlebars Templates
 		
 		this.mkdir('templates');
 		this.mkdir('templates/views');
-
+		
 		this.directory('templates/default-hbs/views/layouts', 'templates/views/layouts');
 		this.directory('templates/default-hbs/views/helpers', 'templates/views/helpers');
 		this.directory('templates/default-hbs/views/partials', 'templates/views/partials');
-
-
+		
+		
 		this.copy('templates/default-hbs/views/index.hbs', 'templates/views/index.hbs');
-
+		
 		if (this.includeBlog) {
 			this.copy('templates/default-hbs/views/blog.hbs', 'templates/views/blog.hbs');
 			this.copy('templates/default-hbs/views/post.hbs', 'templates/views/post.hbs');
 		}
-
+		
 		if (this.includeGallery) {
 			this.copy('templates/default-hbs/views/gallery.hbs', 'templates/views/gallery.hbs');
 		}
-
+		
 		if (this.includeEnquiries) {
 			this.copy('templates/default-hbs/views/contact.hbs', 'templates/views/contact.hbs');
 			if (this.includeEmail) {
@@ -373,22 +372,22 @@ KeystoneGenerator.prototype.templates = function templates() {
 		
 		this.mkdir('templates');
 		this.mkdir('templates/views');
-
+		
 		this.directory('templates/default-jade/layouts', 'templates/layouts');
 		this.directory('templates/default-jade/mixins', 'templates/mixins');
 		this.directory('templates/default-jade/views/errors', 'templates/views/errors');
-
+		
 		this.copy('templates/default-jade/views/index.jade', 'templates/views/index.jade');
-
+		
 		if (this.includeBlog) {
 			this.copy('templates/default-jade/views/blog.jade', 'templates/views/blog.jade');
 			this.copy('templates/default-jade/views/post.jade', 'templates/views/post.jade');
 		}
-
+		
 		if (this.includeGallery) {
 			this.copy('templates/default-jade/views/gallery.jade', 'templates/views/gallery.jade');
 		}
-
+		
 		if (this.includeEnquiries) {
 			this.copy('templates/default-jade/views/contact.jade', 'templates/views/contact.jade');
 			if (this.includeEmail) {
@@ -396,17 +395,17 @@ KeystoneGenerator.prototype.templates = function templates() {
 			}
 		}
 	}
-
+	
 };
 
 KeystoneGenerator.prototype.updates = function routes() {
-
+	
 	this.directory('updates');
-
+	
 };
 
 KeystoneGenerator.prototype.files = function files() {
-
+	
 	this.directory('public');
-
+	
 };
