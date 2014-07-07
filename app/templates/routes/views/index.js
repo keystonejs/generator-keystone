727de0d65cf53863ff08c1ef4a937bd4760151d4
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2014-07-07 17:38:27 +1000

    Updated code for consistency

--- app/templates/routes/views/index.js
+++ app/templates/routes/views/index.js
@@ -2,8 +2,8 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 	
-	var locals = res.locals,
-		view = new keystone.View(req, res);
+	var view = new keystone.View(req, res),
+		locals = res.locals;
 	
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
