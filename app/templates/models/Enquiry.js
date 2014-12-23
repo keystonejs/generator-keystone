1e91793b6b4dca7b948ce682c569b5f3ed4d22e4
Jed Watson <jed.watson@me.com>
2014-12-23 23:40:27 +1100

    Cleaning up quotes

--- app/templates/models/Enquiry.js
+++ app/templates/models/Enquiry.js
@@ -16,9 +16,9 @@ Enquiry.add({
 	email: { type: Types.Email, required: true },
 	phone: { type: String },
 	enquiryType: { type: Types.Select, options: [
-		{ value: 'message', label: "Just leaving a message" },
-		{ value: 'question', label: "I've got a question" },
-		{ value: 'other', label: "Something else..." }
+		{ value: 'message', label: 'Just leaving a message' },
+		{ value: 'question', label: 'I\'ve got a question' },
+		{ value: 'other', label: 'Something else...' }
 	] },
 	message: { type: Types.Markdown, required: true },
 	createdAt: { type: Date, default: Date.now }
