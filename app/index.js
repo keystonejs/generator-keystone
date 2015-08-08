f0986b5419ce55e3b8b5ee2a5384ac391eb814a5
Jed Watson <jed.watson@me.com>
2015-08-08 21:48:38 +1000

    Removing mandrill note

--- app/index.js
+++ app/index.js
@@ -7,20 +7,20 @@ var yeoman = require('yeoman-generator');
 
 
 var KeystoneGenerator = module.exports = function KeystoneGenerator(args, options, config) {
-	
+
 	// Set utils for use in templates
 	this.utils = utils;
-	
+
 	// Initialise default values
 	this.cloudinaryURL = false;
 	this.mandrillAPI = false;
-	
+
 	// Apply the Base Generator
 	yeoman.generators.Base.apply(this, arguments);
-	
+
 	// Welcome
 	console.log('\nWelcome to KeystoneJS.\n');
-	
+
 	// This callback is fired when the generator has completed,
 	// and includes instructions on what to do next.
 	var done = _.bind(function done() {
@@ -48,35 +48,35 @@ var KeystoneGenerator = module.exports = function KeystoneGenerator(args, option
 
 			'\n\nTo start your new website, run ' + cmd + '.' +
 			'\n');
-		
+
 	}, this);
-	
+
 	// Install Dependencies when done
 	this.on('end', function () {
-		
+
 		this.installDependencies({
 			bower: false,
 			skipMessage: true,
 			skipInstall: options['skip-install'],
 			callback: done
 		});
-		
+
 	});
-	
+
 	// Import Package.json
 	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-	
+
 };
 
 // Extends the Base Generator
 util.inherits(KeystoneGenerator, yeoman.generators.Base);
 
 KeystoneGenerator.prototype.prompts = function prompts() {
-	
+
 	var cb = this.async();
-	
+
 	var prompts = {
-		
+
 		project: [
 			{
 				name: 'projectName',
@@ -130,29 +130,28 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 				name: 'includeEmail',
 				message: '------------------------------------------------' +
 					'\n    KeystoneJS integrates with Mandrill (from Mailchimp) for email sending.' +
-					'\n    Mandrill accounts are free for up to 12k emails per month.' +
 					'\n    Would you like to include Email configuration in your project?',
 				default: true
 			}
 		],
-		
+
 		config: []
-		
+
 	};
-	
+
 	this.prompt(prompts.project, function(props) {
-		
+
 		_.each(props, function(val, key) {
 			this[key] = val;
 		}, this);
-		
+
 		// Keep an unescaped version of the project name
 		this._projectName = this.projectName;
 		// ... then escape it for use in strings (most cases)
 		this.projectName = utils.escapeString(this.projectName);
 		this.adminLogin = utils.escapeString(this.adminLogin);
 		this.adminPassword = utils.escapeString(this.adminPassword);
-		
+
 		// Clean the viewEngine selection
 		if (_.contains(['handlebars', 'hbs', 'h'], this.viewEngine.toLowerCase().trim())) {
 			this.viewEngine = 'hbs';
@@ -170,11 +169,11 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 		} else {
 			this.preprocessor = 'less';
 		}
-		
+
 		// Clean the userModel name
 		this.userModel = utils.camelcase(this.userModel, false);
 		this.userModelPath = utils.keyToPath(this.userModel, true);
-		
+
 		// Clean the taskRunner selection
 		this.taskRunner = (this.taskRunner || '').toLowerCase().trim();
 
@@ -182,10 +181,10 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 		if (this.newDirectory) {
 			this.destinationRoot(utils.slug(this.projectName));
 		}
-		
+
 		// Additional prompts may be required, based on selections
 		if (this.includeBlog || this.includeGallery || this.includeEmail) {
-			
+
 			if (this.includeEmail) {
 				prompts.config.push({
 					name: 'mandrillAPI',
@@ -198,17 +197,17 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 						'\n    Your Mandrill API Key:'
 				});
 			}
-			
+
 			if (this.includeBlog || this.includeGallery) {
-				
+
 				var blog_gallery = 'blog and gallery templates';
-				
+
 				if (!this.includeBlog) {
 					blog_gallery = 'gallery template';
 				} else if (!this.includeGallery) {
 					blog_gallery = 'blog template';
 				}
-				
+
 				prompts.config.push({
 					name: 'cloudinaryURL',
 					message: '------------------------------------------------' +
@@ -221,43 +220,43 @@ KeystoneGenerator.prototype.prompts = function prompts() {
 						'\n    ' +
 						'\n    Please enter your Cloudinary URL:'
 				});
-				
+
 			}
-			
+
 		}
-		
+
 		if (!prompts.config.length) {
 			return cb();
 		}
-		
+
 		this.prompt(prompts.config, function(props) {
-			
+
 			_.each(props, function(val, key) {
 				this[key] = val;
 			}, this);
-			
+
 			if (this.includeEmail && !this.mandrillAPI) {
 				this.usingTestMandrillAPI = true;
 				this.mandrillAPI = 'NY8RRKyv1Bure9bdP8-TOQ';
 			}
-			
+
 			if (!this.cloudinaryURL && (this.includeBlog || this.includeGallery)) {
 				this.usingDemoCloudinaryAccount = true;
 				this.cloudinaryURL = 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo';
 			}
-			
+
 			cb();
-			
+
 		}.bind(this));
-		
+
 	}.bind(this));
-	
+
 };
 
 KeystoneGenerator.prototype.guideComments = function() {
-	
+
 	var cb = this.async();
-	
+
 	this.prompt([
 		{
 			type: 'confirm',
@@ -268,183 +267,183 @@ KeystoneGenerator.prototype.guideComments = function() {
 			default: true
 		}
 	], function(props) {
-		
+
 		this.includeGuideComments = props.includeGuideComments;
 		cb();
-		
+
 	}.bind(this));
-	
+
 };
 
 KeystoneGenerator.prototype.keys = function keys() {
-	
+
 	var cookieSecretChars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz!@#$%^&*()-=_+[]{}|;:",./<>?`~';
-	
+
 	this.cookieSecret = utils.randomString(64, cookieSecretChars);
-	
+
 };
 
 KeystoneGenerator.prototype.project = function project() {
-	
+
 	this.template('_package.json', 'package.json');
 	this.template('_env', '.env');
 	this.template('_jshintrc', '.jshintrc');
-	
+
 	this.template('_keystone.js', 'keystone.js');
-	
+
 	this.copy('editorconfig', '.editorconfig');
 	this.copy('gitignore', '.gitignore');
 	this.copy('Procfile');
-	
+
 	if (this.taskRunner === 'grunt') {
 		this.copy('Gruntfile.js');
 		this.directory('grunt', 'grunt');
 	}
-	
+
 	if (this.taskRunner === 'gulp'){
 		this.copy('gulpfile.js');
 	}
-	
+
 };
 
 KeystoneGenerator.prototype.models = function models() {
-	
+
 	var modelFiles = [];
-	
+
 	if (this.includeBlog) {
 		modelFiles.push('Post');
 		modelFiles.push('PostCategory');
 	}
-	
+
 	if (this.includeGallery) {
 		modelFiles.push('Gallery');
 	}
-	
+
 	if (this.includeEnquiries) {
 		modelFiles.push('Enquiry');
 	}
-	
+
 	this.mkdir('models');
-	
+
 	this.template('models/_User.js', 'models/' + this.userModel + '.js');
-	
+
 	modelFiles.forEach(function(i) {
 		this.template('models/' + i + '.js');
 	}, this);
-	
+
 };
 
 KeystoneGenerator.prototype.routes = function routes() {
-	
+
 	this.mkdir('routes');
 	this.mkdir('routes/views');
-	
+
 	this.template('routes/_index.js', 'routes/index.js');
 	this.template('routes/_middleware.js', 'routes/middleware.js');
-	
+
 	if (this.includeEmail) {
 		this.template('routes/_emails.js', 'routes/emails.js');
 	}
-	
+
 	this.copy('routes/views/index.js');
-	
+
 	if (this.includeBlog) {
 		this.copy('routes/views/blog.js');
 		this.copy('routes/views/post.js');
 	}
-	
+
 	if (this.includeGallery) {
 		this.copy('routes/views/gallery.js');
 	}
-	
+
 	if (this.includeEnquiries) {
 		this.copy('routes/views/contact.js');
 	}
-	
+
 };
 
 KeystoneGenerator.prototype.templates = function templates() {
-	
+
 	if (this.viewEngine === 'hbs') {
-		
+
 		// Copy Handlebars Templates
-		
+
 		this.mkdir('templates');
 		this.mkdir('templates/views');
-		
+
 		this.directory('templates/default-hbs/views/layouts', 'templates/views/layouts');
 		this.directory('templates/default-hbs/views/helpers', 'templates/views/helpers');
 		this.directory('templates/default-hbs/views/partials', 'templates/views/partials');
-		
+
 		this.template('templates/default-hbs/views/index.hbs', 'templates/views/index.hbs');
-		
+
 		if (this.includeBlog) {
 			this.copy('templates/default-hbs/views/blog.hbs', 'templates/views/blog.hbs');
 			this.copy('templates/default-hbs/views/post.hbs', 'templates/views/post.hbs');
 		}
-		
+
 		if (this.includeGallery) {
 			this.copy('templates/default-hbs/views/gallery.hbs', 'templates/views/gallery.hbs');
 		}
-		
+
 		if (this.includeEnquiries) {
 			this.copy('templates/default-hbs/views/contact.hbs', 'templates/views/contact.hbs');
 			if (this.includeEmail) {
 				this.copy('templates/default-hbs/emails/enquiry-notification.hbs', 'templates/emails/enquiry-notification.hbs');
 			}
 		}
-		
+
 	} else if (this.viewEngine === 'nunjucks') {
-		
+
 		// Copy Nunjucks Templates
-		
+
 		this.mkdir('templates');
 		this.mkdir('templates/views');
-		
+
 		this.directory('templates/default-' + this.viewEngine + '/layouts', 'templates/layouts');
 		this.directory('templates/default-' + this.viewEngine + '/mixins', 'templates/mixins');
 		this.directory('templates/default-' + this.viewEngine + '/views/errors', 'templates/views/errors');
-		
+
 		this.template('templates/default-' + this.viewEngine + '/views/index.html', 'templates/views/index.html');
-		
+
 		if (this.includeBlog) {
 			this.copy('templates/default-' + this.viewEngine + '/views/blog.html', 'templates/views/blog.html');
 			this.copy('templates/default-' + this.viewEngine + '/views/post.html', 'templates/views/post.html');
 		}
-		
+
 		if (this.includeGallery) {
 			this.copy('templates/default-' + this.viewEngine + '/views/gallery.html', 'templates/views/gallery.html');
 		}
-		
+
 		if (this.includeEnquiries) {
 			this.copy('templates/default-' + this.viewEngine + '/views/contact.html', 'templates/views/contact.html');
 			if (this.includeEmail) {
 				this.directory('templates/default-' + this.viewEngine + '/emails', 'templates/emails');
 			}
 		}
-		
+
 	} else {
-		
+
 		// Copy Jade/Swig Templates
-		
+
 		this.mkdir('templates');
 		this.mkdir('templates/views');
-		
+
 		this.directory('templates/default-' + this.viewEngine + '/layouts', 'templates/layouts');
 		this.directory('templates/default-' + this.viewEngine + '/mixins', 'templates/mixins');
 		this.directory('templates/default-' + this.viewEngine + '/views/errors', 'templates/views/errors');
-		
+
 		this.template('templates/default-' + this.viewEngine + '/views/index.' + this.viewEngine, 'templates/views/index.' + this.viewEngine);
-		
+
 		if (this.includeBlog) {
 			this.copy('templates/default-' + this.viewEngine + '/views/blog.' + this.viewEngine, 'templates/views/blog.' + this.viewEngine);
 			this.copy('templates/default-' + this.viewEngine + '/views/post.' + this.viewEngine, 'templates/views/post.' + this.viewEngine);
 		}
-		
+
 		if (this.includeGallery) {
 			this.copy('templates/default-' + this.viewEngine + '/views/gallery.' + this.viewEngine, 'templates/views/gallery.' + this.viewEngine);
 		}
-		
+
 		if (this.includeEnquiries) {
 			this.copy('templates/default-' + this.viewEngine + '/views/contact.' + this.viewEngine, 'templates/views/contact.' + this.viewEngine);
 			if (this.includeEmail) {
@@ -452,17 +451,17 @@ KeystoneGenerator.prototype.templates = function templates() {
 			}
 		}
 	}
-	
+
 };
 
 KeystoneGenerator.prototype.updates = function routes() {
-	
+
 	this.directory('updates');
-	
+
 };
 
 KeystoneGenerator.prototype.files = function files() {
-	
+
 	this.directory('public/fonts');
 	this.directory('public/images');
 	this.directory('public/js');
@@ -473,5 +472,5 @@ KeystoneGenerator.prototype.files = function files() {
 	} else {
 		this.directory('public/styles-less', 'public/styles');
 	}
-	
+
 };
