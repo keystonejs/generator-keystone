73ad49aa58d9d0ebcf40fea88d76d17a68ba5b21
Jed Watson <jed.watson@me.com>
2017-02-17 14:13:41 +1100

    Adding unique: true to the User model, closes #102

--- app/templates/models/_User.js
+++ app/templates/models/_User.js
@@ -9,7 +9,7 @@ var <%= userModel %> = new keystone.List('<%= userModel %>');
 
 <%= userModel %>.add({
 	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
+	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
