a8308262ac5b446afbc198a8a383742e1eb9ffbe
Przemysław Tomaszewski <firian@o2.pl>
2017-03-17 23:53:40 +0100

    Fix for "Posts has no field postCategory" error
    It's related to https://github.com/keystonejs/keystone/issues/1870.

--- app/templates/models/PostCategory.js
+++ app/templates/models/PostCategory.js
@@ -13,6 +13,6 @@ PostCategory.add({
 	name: { type: String, required: true },
 });
 
-PostCategory.relationship({ ref: 'Post', path: 'categories' });
+PostCategory.relationship({ ref: 'Post', path: 'posts', refPath: 'categories' });
 
 PostCategory.register();
